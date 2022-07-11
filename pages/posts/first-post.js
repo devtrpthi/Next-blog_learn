55bc4915654fb8ea340964aab94df15631d7bf1b
devtrpthi <deveshkrishnatripathi@gmail.com>
2022-07-11 16:46:40 +0530

    link-to-home

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,3 +1,15 @@
-export default function Firstpost() {
-    return <h1>First Post</h1>;
+import Link from "next/link";
+
+export default function FirstPost() {
+    return(
+        <>
+            <h1>First Post
+            <h2>
+                <Link href="/">
+                    <a>Back to Home</a>
+                </Link>
+            </h2>
+            </h1>
+        </>
+    );
 }
