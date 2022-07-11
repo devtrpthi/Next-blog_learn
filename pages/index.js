24f2d681444863e71b2919c4ab1edf2f080ed83c
devtrpthi <deveshkrishnatripathi@gmail.com>
2022-07-11 23:46:36 +0530

    styles.module

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import Layout,{siteTitle} from '../components/layout';
-import utilStyles from '../styles/utils.modules.css';
+import utilStyles from '../styles/utils.module.css';
 
 export default function Home() {
   return (
